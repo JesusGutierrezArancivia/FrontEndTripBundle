ce34c10e4d7addf108b1e7c87d46c4f534bcd3ee
Jesús Andrés Gutiérrez Arancivia <jesus.guti.arancivia@outlook.com>
2024-11-17 21:31:40 -0500

    Security y API implementados
    Desarrollo de la API y actualización del listar de ciudades a Card

--- src/app/components/city/listar/listar.component.ts
+++ src/app/components/city/listar/listar.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { City } from '../../../models/City';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table'
 import { CityService } from '../../../services/city.service';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
-
+import {GoogleMap, GoogleMapsModule, MapAdvancedMarker} from '@angular/google-maps';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'app-listar',
   standalone: true,
-  imports: [MatTableModule, MatIconModule,RouterModule],
+  imports: [MatTableModule, MatIconModule,RouterModule,GoogleMap,GoogleMapsModule,MapAdvancedMarker, CommonModule,MatCardModule],
   templateUrl: './listar.component.html',
   styleUrl: './listar.component.css'
 })
 export class ListarComponent implements OnInit{
   datasource: MatTableDataSource<City> = new MatTableDataSource();
   displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6','accion01','accion02'];
-
+  cities: City[] = [];
+  selectedCity: City | null = null;
   constructor(private cS: CityService) {}
   ngOnInit(): void {
     this.cS.list().subscribe((data) => {
@@ -23,6 +26,9 @@ export class ListarComponent implements OnInit{
     });
     this.cS.getList().subscribe((data) => {
       this.datasource = new MatTableDataSource(data);
+    });
+    this.cS.getCities().subscribe((data: City[]) => {
+      this.cities = data;
     });
   }
 
@@ -33,4 +39,34 @@ export class ListarComponent implements OnInit{
       });
     });
   }
+  center: google.maps.LatLngLiteral = {lat: -9.19, lng: -75.0152};
+  zoom = 6;
+  showCityDetails(city: City) {
+    this.selectedCity = city;
+    this.center = { lat: city.latitudeCity, lng: city.lengthCity }; // Centra mapa
+    this.zoom = 10; 
+  }
+
+
+  $cities=signal<City[]>([
+    {
+      idCity:1,
+      nameCity: 'Lima',
+      latitudeCity:-12.0464,
+      lengthCity:-77.0428,
+      provinceCity:'Lima',
+      departmentCity:'Lima'
+    }
+  ])
+  /*
+  limaPosition={lat:-12.0464, lng:-77.0428};
+
+  options: google.maps.MapOptions={
+    center:this.limaPosition,
+    zoom:17,
+  }
+  markerOptions: google.maps.marker.AdvancedMarkerElementOptions={
+    position:this.limaPosition
+  }
+  */
 }
